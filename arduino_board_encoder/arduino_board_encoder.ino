951a848b48011e073e3335f96dd85fa5177fec24
Jay Morgan <jaymorgan@Jays-MacBook-Pro.local>
2014-03-06 15:01:07 +0000

    Modified rotary encoder. Using software pull-ups (digitalWrite(EncoderPinA, HIGH);). Using interrupt method. Rotary encoder now modifies x coordinate position of mouse.

--- arduino_board_encoder/arduino_board_encoder.ino
+++ arduino_board_encoder/arduino_board_encoder.ino
@@ -1,5 +1,3 @@
-#include <Bounce.h>
-
 const int buttonRight = 10;       // Push Switch Left
 const int buttonLeft = 11;        // Push Switch right
 const int buttonUp = 12;         // Push Switch Up
@@ -11,32 +9,22 @@ const int EncoderInterrupt = 0;  // interrupt feature, might replace with pollin
 //const int greLED = 8; 
 //const int bluLED = 9;
 //const int SW = 10;               // push down feature on the rotary encoder
-const int LED = 13;              // standard LED pin
-volatile int buttonState = 0;    // state of button
-volatile int _encoderTicks = 0;
-
-//Software Debouncers
-//Bounce debounceBLeft = Bounce(buttonLeft, 1);   // 1 milliseconds delay, good switches
-//Bounce debounceBRight = Bounce(buttonRight, 1);
-//Bounce debounceBUp = Bounce(buttonUp, 1);   // 1 milliseconds delay, good switches
-//Bounce debounceBDown = Bounce(buttonDown, 1);
-//Bounce pushSwitch = Bounce(SW, 5);
+const int LED = 13;
 
 void setup() { 
   Serial.begin(9600);
     pinMode(EncoderPinA, INPUT); //set pin A and pin B as inputs
+    digitalWrite(EncoderPinA, HIGH); // software pull up
     pinMode(EncoderPinB, INPUT);
+    digitalWrite(EncoderPinB, HIGH);
     pinMode(buttonLeft, INPUT);
     pinMode(buttonRight, INPUT);
    // pinMode(SW, INPUT);
    
-    //attachInterrupt(EncoderInterrupt, HandleEncoderInterrupt, CHANGE); //add interrupt for any change in pins
+    attachInterrupt(0, HandleEncoderInterrupt, CHANGE); //add interrupt for any change in pins
 }
 
 void loop() {
-  //debounceBLeft.update();  //update debouncers
-  //debounceBRight.update();
-  //pushSwitch.update(); 
   
   if (digitalRead(buttonLeft) == HIGH) { 
       //output
@@ -72,37 +60,15 @@ void loop() {
     Keyboard.releaseAll();
     analogWrite(greLED, HIGH); 
   }*/
-  
-  
-  
-  /*if (digitalRead(EncoderPinA) == LOW) { 
-     turnRight(); 
-  } 
-  
-  if (digitalRead(EncoderPinB) == LOW) { 
-     turnLeft(); 
-  }*/
          
   delay(10);
 }
     
     
 void HandleEncoderInterrupt() { 
-  //_encoderTicks = ((PIND >> 1) & 0x1) ? 1 : 2 ;
-  
-  //_encoderTicks = 0;
-}
-
-void turnLeft() { 
-      Keyboard.begin();
-      Keyboard.press(216);
-      Keyboard.releaseAll();
-      //analogWrite(redLED, 1); 
-}
-
-void turnRight() { 
-   Keyboard.begin();
-      Keyboard.press(215);
-      Keyboard.releaseAll(); 
-      //analogWrite(bluLED, 1);
+  if (digitalRead(EncoderPinA) == digitalRead(EncoderPinB)) {
+    Mouse.move(10,0,0);
+  } else {
+    Mouse.move(-10,0,0);
+  }
 }
