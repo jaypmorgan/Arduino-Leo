e5a90d036e6d9c19258ad79bd3d4ce433ac17c5e
jaypmorgan <jaymiles17@gmail.com>
2014-03-25 17:44:01 +0000

    Added imp for SW

--- arduino_board_encoder/arduino_board_encoder.ino
+++ arduino_board_encoder/arduino_board_encoder.ino
@@ -1,74 +1,97 @@
-const int buttonRight = 10;       // Push Switch Left
-const int buttonLeft = 11;        // Push Switch right
-const int buttonUp = 12;         // Push Switch Up
-const int buttonDown = 9;       // Push Switch Down
-const int EncoderPinA = 2;       // knob turn left
-const int EncoderPinB = 3;       // knob turn right
+#include <Bounce.h>
+
+const int buttonRight = 9;       // Push Switch Left
+const int buttonLeft = 12;        // Push Switch right
+const int buttonUp = 13;         // Push Switch Up
+const int buttonDown = 11;       // Push Switch Down
+const int EncoderPinA = 3;       // knob turn left
+const int EncoderPinB = 2;       // knob turn right
 const int EncoderInterrupt = 0;  // interrupt feature, might replace with polling tech
+const int SW = 5;               // push down feature on the rotary encoder
 //const int redLED = 7; 
 //const int greLED = 8; 
 //const int bluLED = 9;
-//const int SW = 10;               // push down feature on the rotary encoder
-const int LED = 13;
+
+Bounce bouncerPinA = Bounce(20, EncoderPinA);
+Bounce bouncerPinB = Bounce(20, EncoderPinB);
 
 void setup() { 
-  Serial.begin(9600);
+    Serial.begin(9600);
+    
     pinMode(EncoderPinA, INPUT); //set pin A and pin B as inputs
-    digitalWrite(EncoderPinA, HIGH); // software pull up
     pinMode(EncoderPinB, INPUT);
+    pinMode(SW, INPUT);
+    digitalWrite(EncoderPinA, HIGH); // software pull up
     digitalWrite(EncoderPinB, HIGH);
+    
+    // Push Buttons 
+    pinMode(buttonUp, INPUT);
+    pinMode(buttonDown, INPUT);
     pinMode(buttonLeft, INPUT);
     pinMode(buttonRight, INPUT);
-   // pinMode(SW, INPUT);
+    
+    digitalWrite(buttonUp, HIGH);
+    digitalWrite(buttonDown, HIGH);
+    digitalWrite(buttonLeft, HIGH);
+    digitalWrite(buttonRight, HIGH);
    
     attachInterrupt(0, HandleEncoderInterrupt, CHANGE); //add interrupt for any change in pins
-}
+    //attachInterrupt(1, SwitchInterrupt, LOW);
+}                                                                                                   
 
 void loop() {
   
-  if (digitalRead(buttonLeft) == HIGH) { 
+  if (digitalRead(buttonLeft) == LOW) { 
       //output
       Keyboard.begin();
       Keyboard.press(216); // left arrow
       Keyboard.releaseAll();
   } 
   
-  if (digitalRead(buttonRight) == HIGH) { 
+  if (digitalRead(buttonRight) == LOW) { 
     //output  
     Keyboard.begin();
     Keyboard.press(215); // right arrow
     Keyboard.releaseAll();
   }
   
-  if (digitalRead(buttonUp) == HIGH) { 
+  if (digitalRead(buttonUp) == LOW) { 
       //output
       Keyboard.begin();
       Keyboard.press(218); // up arrow
-      Keyboard.releaseAll();
+      Keyboard.releaseAll(); 
   } 
   
-  if (digitalRead(buttonDown) == HIGH) { 
+  if(digitalRead(buttonDown) == LOW) { 
     //output  
     Keyboard.begin();
     Keyboard.press(217); // down arrow
     Keyboard.releaseAll();
   }
   
-  /*if (pushSwitch.read() == HIGH) {
+  if (digitalRead(SW) == HIGH) {
     Keyboard.begin();
-    Keyboard.press(32); // page up
+    Keyboard.press('f'); // down arrow
     Keyboard.releaseAll();
-    analogWrite(greLED, HIGH); 
-  }*/
-         
+    
+  } 
+
+  Serial.println(digitalRead(SW));  
   delay(10);
 }
     
     
 void HandleEncoderInterrupt() { 
+  bouncerPinA.update();
+  bouncerPinB.update();
+  
+  Serial.println(digitalRead(EncoderPinA));
+  Serial.println(digitalRead(EncoderPinB));
+  
+  
   if (digitalRead(EncoderPinA) == digitalRead(EncoderPinB)) {
-    Mouse.move(10,0,0);
-  } else {
     Mouse.move(-10,0,0);
+  } else  {
+    Mouse.move(10,0,0);
   }
 }
